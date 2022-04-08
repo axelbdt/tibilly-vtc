44bd96854cd626b8a90d10f672eb6f43c1954f17
Axel Baudot <axel.baudot@protonmail.com>
2022-04-08 15:55:56 +0200

    add wkhtmltopfd dependency and helpers

---
+++ Application/Helper/Wkhtmltopdf.hs
@@ -0,0 +1,31 @@
+module Application.Helper.Wkhtmltopdf where
+
+import System.Process
+import GHC.IO.Handle
+import Control.Monad
+import Text.Blaze.Html (Html)
+import Text.Blaze.Html.Renderer.Utf8 (renderHtml)
+import qualified Data.ByteString as B
+import qualified Data.ByteString.Lazy as BL
+import qualified System.Info as SI
+import Data.Text as T (unpack)
+import IHP.Prelude
+
+convertBytestring :: LByteString -> IO B.ByteString
+convertBytestring bs = do
+    (Just stdin, Just stdout, _, _) <- createProcess cprocess
+    BL.hPutStr stdin bs >> hClose stdin
+    B.hGetContents stdout
+    where
+        procWith p = p { std_out = CreatePipe
+                       , std_in  = CreatePipe 
+                       }
+
+        cprocess = procWith $ proc "wkhtmltopdf" opts
+
+
+        opts = map T.unpack ["--encoding", "utf-8" , "-", "-"]
+
+-- Convert the given html and return the pdf as a strict bytestring.
+convertHtml :: Html -> IO B.ByteString
+convertHtml = convertBytestring . renderHtml
