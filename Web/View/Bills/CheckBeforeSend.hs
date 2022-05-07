82c2b5f0f47a2e4c8e69d9401abbed96d2bba156
Axel Baudot <axel.baudot@protonmail.com>
2022-05-07 15:12:42 +0200

    Revert "Add a check button, to check in prod on a mobile device"
    This reverts commit 7d2db57233e682a45b096a2fef39108b79cdebe1.

--- Web/View/Bills/CheckBeforeSend.hs
+++ Web/View/Bills/CheckBeforeSend.hs
@@ -6,39 +6,15 @@ data CheckBeforeSendView = CheckBeforeSendView { bill :: Bill }
 
 instance View CheckBeforeSendView where
     html CheckBeforeSendView { .. } = [hsx|
-        <script data-pdf-url={pdfUrl}>
-            const pdfUrl = document.currentScript.dataset.pdfUrl;
-            function share() {
-              fetch(pdfUrl)
-                .then(function(response) {
-                  return response.blob()
-                })
-                .then(function(blob) {
-
-                  var file = new File([blob], "picture.jpg", {type: 'image/jpeg'});
-                  var filesArray = [file];
-
-                  if(navigator.canShare && navigator.canShare({ files: filesArray })) {
-                    navigator.share({
-                      files: filesArray,
-                    });
-                  }
-                  else {
-                      console.log("Sharing is not supported.")
-                    }
-                })
-            }
-        </script>
         <h1>Téléchargez votre facture</h1>
         <div class="w-100 my-3" style="height:32em">
-            <iframe class="w-100 h-100" src={pdfUrl} title="Bill" allowfullscreen></iframe>
+            <iframe class="w-100 h-100" src={pathTo (GenerateBillPDFAction (get #id bill) billNumber sentOnText)} title="Bill" allowfullscreen></iframe>
         </div>
         {renderForm bill} 
         |]
             where
                 sentOnText = maybe "" show (get #sentOn bill)
                 billNumber = fromMaybe 0 (get #number bill)
-                pdfUrl = pathTo (GenerateBillPDFAction (get #id bill) billNumber sentOnText)
 
 
 renderForm bill = formFor' bill (pathTo (SendBillAction (get #id bill))) [hsx|
@@ -46,7 +22,7 @@ renderForm bill = formFor' bill (pathTo (SendBillAction (get #id bill))) [hsx|
     {hiddenField #number}
     <div class="d-flex justify-content-between mt-3">
         <a href={pathTo (ShowBillAction (get #id bill))} class="btn btn-outline-primary">Retour</a>
-        <button type="button" class="btn btn-primary" onclick="share()">Partager</button>
         {submitButton { label = "Enregistrer"}}
     </div>
+
 |]
